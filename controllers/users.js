5de00f860953b32dffc93ebf49e7ffbad6de9b44
joecruz17 <joejcruz17@gmail.com>
2023-06-24 11:49:39 -0400

    more things

--- controllers/users.js
+++ controllers/users.js
@@ -8,10 +8,37 @@ exports.auth = async = async (req, res, next ) => {
     try {
         const token = req.header('Authorization').replace('Bearer', '')
         const data = jwt.verify(token, process.env.SECRET)
-        const user = await User.findOne({ _id: data._idn})
+        const user = await User.findOne({ _id: data._idn}) 
+        if(!user){
+            throw new Error('bad credentials')
+        }
+        req.user = user
+        next()
     } catch (error) {
+        res.status(401).json({ message: error.message})
+        
+    }
+}
+
+exports.createUser = asycn (req, res) => {
+    try {
+        const user = new User(req.body)
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.json({ user, token })
+    } catch (error) {
+        res.status(400).json({ message: error.message})
         
     }
 }
 
 //acts as middleware to check if user is authoried to do somehting
+
+exports.loginUser = async (req. res) => {
+    try {
+        const user = await user.findOne({ email: req.body.email})
+        
+    } catch (error) {
+        
+    }
+}
