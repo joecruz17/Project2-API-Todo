77896715a70a417c5cd7acb5eae949ee5a7bf6c8
joecruz17 <joejcruz17@gmail.com>
2023-07-06 12:38:50 -0400

    tests

--- routes/todos.js
+++ routes/todos.js
@@ -3,16 +3,10 @@ const router = express.Router()
 const todoCtrl = require('../controllers/todos')
 const userController = require('../controllers/users')
 
-router.get('/', userController.auth, todoCtrl.indexNotComplete)
-
+router.get('/incomplete', userController.auth, todoCtrl.indexNotComplete)
 router.get('/completed', userController.auth, todoCtrl.indexComplete)
-
 router.delete('/:id', userController.auth, todoCtrl.delete)
-
 router.put('/:id', userController.auth, todoCtrl.update)
-
 router.post('/', userController.auth, todoCtrl.create)
-
 router.get('/:id', userController.auth, todoCtrl.show)
-
 module.exports = router
